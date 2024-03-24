c560b6c3d76c48bd91537c94cff2b945a42f04ad
Sumon Hossen <hossensumon071@gmail.com>
2024-03-25 05:18:30 +0600

    mongodb connected

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-require("dotenv").config()
-const express = require('express')
-const projectRoutes = require("./routes/projectRoute")
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const projectRoutes = require("./routes/projectRoute");
 
-// express app 
-const app = express()
+// express app
+const app = express();
 
-// port 
-const port = process.env.PORT || 4000
+// port
+const port = process.env.PORT || 4000;
 
 // middlewares
-app.use(express.json())
+app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
-// routes 
-app.use("/api/projects", projectRoutes)
+// routes
+app.use("/api/projects", projectRoutes);
 
-// listen for request 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-}) 
+// mongodb
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for request
+    app.listen(port, () => {
+      console.log(`connected to mongo and listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
